9c8bbcf56cf2ba151a8b8827d2c3fe4474fe0472
Jashan <jashandeep4329@gmail.com>
2025-10-22 23:56:59 +0530

    Add capitalize function and corresponding test

---
+++ __tests__/capitalize.test.js
@@ -0,0 +1,4 @@
+test ("Check if the first letter of the string is Capitalized", () => {
+
+    expect (capitalize("hello")).toBe("Hello");
+})
--- example.test.js
+++
@@ -1,6 +0,0 @@
-const add = (a, b) => a + b;
-
-test ("adds two integers", () => {
-
-    expect(add(5, 5)).toBe(10);
-})
