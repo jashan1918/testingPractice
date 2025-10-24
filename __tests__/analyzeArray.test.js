1a9e4e89c147e5c864c25242e78736e933afb391
Jashan <jashandeep4329@gmail.com>
2025-10-24 23:05:03 +0530

    feat: implement analyzeArray function to calculate average, min, max, and length of an array

--- __tests__/analyzeArray.test.js
+++ __tests__/analyzeArray.test.js
@@ -4,6 +4,15 @@ describe ("Tests for analyzeArray function", () => {
     
     test("To Test the analyzeArray function with generic data", () => {
 
-        expect(analyzeArray()).toBe(10);
+        expect(analyzeArray([1,8,3,4,2,6])).toEqual({average: 4, min: 1, max: 8, length: 6});
+    })
+
+    test("to test with a single value array0", () => {
+
+        expect(analyzeArray([5])).toEqual({average: 5, min: 5, max: 5, length: 1})
+    })
+
+    test("test when the array sent is empty", () => {
+        expect(() => analyzeArray([])).toThrow("Array cannot be empty")
     })
 })
--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,6 +1,37 @@
 function analyzeArray (arr) {
 
-    return 10;
+    //if array is empty then thrwo an error
+    if(arr.length === 0) throw new Error("Array cannot be empty")
+
+    //find the average
+    let avg;
+    let sum = 0;
+    let min = arr[0];
+    let max = arr[0];
+
+    arr.forEach((e) => {
+        sum += e;
+        if(e < min) {
+            min = e;
+        }
+
+        if(e > max) {
+            max = e;
+        }
+    })
+    avg = sum / arr.length;
+
+    //find the min
+
+    const obj = {
+
+        average: avg,
+        min: min,
+        max: max,
+        length: arr.length
+    }
+
+    return obj;
 
 }
 
